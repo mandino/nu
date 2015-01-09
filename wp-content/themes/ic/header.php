b383408203a843d1588072610b3944d930ba40e7
Mandino <dino@misfit-inc.com>
2015-01-09 15:03:52 +0800

    remove noindex nofollow from author pages

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -12,7 +12,6 @@
 <!DOCTYPE HTML>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<?php if(is_author()) { ?><meta name="robots" content="noindex, nofollow"><?php } ?>
 	<title>
 		<?php global $page, $paged; wp_title( '|', true, 'right' );
 	
