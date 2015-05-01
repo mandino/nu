0592bf61794de69319af9d4ac6ad0b791b64353b
latitudecode <sambakersalmon@gmail.com>
2015-05-01 10:12:02 -0400

    removing blue ic banner

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -196,7 +196,7 @@ pl.src = 'https://beacon.sojern.com/pixel/p/3032';(document.getElementsByTagName
 						</ul> -->
 	
 					</li>
-					<li class="blue-btn"><a href="http://nuhotelbrooklyn.com/why-blue/"><i class="fa fa-info-circle"></i><span class="blue-mobile">why blue?</span></a></li>
+					<!-- <li class="blue-btn"><a href="http://nuhotelbrooklyn.com/why-blue/"><i class="fa fa-info-circle"></i><span class="blue-mobile">why blue?</span></a></li> -->
 				</ul>
 			</nav>
 			
