3be074bfc86f1213a9257a45f30650f55f72c22a
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-07-28 11:19:40 +0800

    added thumb for images 3

--- wp-content/themes/ic/library/featured.php
+++ wp-content/themes/ic/library/featured.php
@@ -9,7 +9,7 @@
 			$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full");?>
 			
 			
-			<li style="background-image: url(<?php echo $imgsrc[0]; ?>);">
+			<li style="background-image: url(<?php echo tt($imgsrc[0], 1400, 498); ?>);">
 				<a target="_blank" href="<?php echo get_post_meta($post->ID, 'sliderurl', true); ?>">
 					<div class="slide-header">
 						
