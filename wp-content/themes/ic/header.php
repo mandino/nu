52347f1c78b19b5f9f13812ac3ffd591ca93e029
jomel <jomel_bola@yahoo.com>
2016-03-25 18:26:50 +0800

    GA CODE

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -111,19 +111,15 @@ pl.src = 'https://beacon.sojern.com/pixel/p/3032';(document.getElementsByTagName
 </script>
 <!-- End Sojern -->
 
-<script>
+<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
 
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-24686149-1', 'auto', {
-		'allowLinker': true
-	});
-	ga('send', 'pageview');
-	ga('require', 'linker');
-	ga('linker:autoLink', ['reztrip.com'], false, true);
+ga('create', 'UA-24686149-1', 'auto',{'allowLinker': true });
+
+ga('require', 'linker'); ga('linker:autoLink', ['nuhotelbrooklyn.reztrip.com','nuhotelbrooklyn.reztripmobile.com']);
+
+ga('send', 'pageview');
 
 </script>
 
