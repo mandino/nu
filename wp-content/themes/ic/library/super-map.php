b3b2b244759068f1292ee095e892fb53eacbdc98
Jhelline <jhelline.fausto@outsourced.ph>
2016-05-24 22:27:45 +0800

    remove conflict

--- wp-content/themes/ic/library/super-map.php
+++ wp-content/themes/ic/library/super-map.php
@@ -539,43 +539,7 @@
     
 </script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/basic.js"></script>
-<<<<<<< HEAD
-<!--
-<script src="<?php bloginfo('template_url'); ?>/js/jquery.colorbox.js"></script>
-  <script>
-    $(document).ready(function(){
-      //Examples of how to assign the ColorBox event to elements
-      $("a[rel='example1']").colorbox();
-      $("a[rel='example2']").colorbox({transition:"fade"});
-      $("a[rel='example3']").colorbox({transition:"none", width:"75%", height:"75%"});
-      $("a[rel='example4']").colorbox({slideshow:true});
-      $(".example5").colorbox();
-      $(".example6").colorbox({iframe:true, innerWidth:425, innerHeight:344});
-      $(".example7").colorbox({width:"80%", height:"80%", iframe:true});
-      $(".example8").colorbox({width:"50%", inline:true, href:"#inline_example1"});
-      $(".example9").colorbox({
-        onOpen:function(){ alert('onOpen: colorbox is about to open'); },
-        onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
-        onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
-        onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-        onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
-      });
-      
-      //Example of preserving a JavaScript event for inline calls.
-      $("#click").click(function(){ 
-        $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-        return false;
-      });
-    });
-  </script>
--->
+
   <!--[if lt IE 7 ]>
   <script src="js/libs/dd_belatedpng.js"></script>
   <script> DD_belatedPNG.fix('img, .png_bg');</script>
-  <![endif]-->
-=======
-	<!--[if lt IE 7 ]>
-	<script src="js/libs/dd_belatedpng.js"></script>
-	<script> DD_belatedPNG.fix('img, .png_bg');</script>
-	<![endif]-->
->>>>>>> seo-update-jhel
