ae1eadb3cb7d8427a5041d30f49449ee9637d236
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2017-06-16 14:47:39 +0800

    remove ga code

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -89,18 +89,6 @@
 	</script>
 	<!-- End Sojern -->
 
-	<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
-
-	Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-24686149-1', 'auto',{'allowLinker': true });
-
-	ga('require', 'linker'); ga('linker:autoLink', ['nuhotelbrooklyn.reztrip.com','nuhotelbrooklyn.reztripmobile.com']);
-
-	ga('send', 'pageview');
-
-	</script>
-
 	<script type="application/ld+json">
 		{
 		"@context": "//schema.org",
