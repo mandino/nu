41e278804da23d53f02e7581c5d5e6c047331577
aljem-mark <speakout.aljem@gmail.com>
2017-06-29 23:19:07 +0800

    gtm implementation

--- wp-content/themes/ic/footer.php
+++ wp-content/themes/ic/footer.php
@@ -158,25 +158,6 @@ js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-<!-- Remarketing -->
-<?php if(is_home()) { ?> 
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 971943105;
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/971943105/?value=0&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
-<?php } ?>
-<!-- End Remarketing -->
-
 <!-- Email signup popup -->
 <?php wp_reset_query(); ?>
 <?php if ( is_single() || is_page( 'blog' ) ) { ?>
