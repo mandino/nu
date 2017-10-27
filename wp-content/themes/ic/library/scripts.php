20611dca202dcab7fc3a4757dca99e8204dba2dc
arvinsumohid <arvinsumohid99@gmail.com>
2017-10-27 13:37:09 +0800

    update homepage video banner

--- wp-content/themes/ic/library/scripts.php
+++ wp-content/themes/ic/library/scripts.php
@@ -562,7 +562,8 @@
 			if ($(window).width() > 768 ) {
 				$('#home-slider.home-video').css('height', 'calc(' + $(window).height() + 'px - ' + ($('.section-header').height() + $('#primary-nav').height() + $('#property-nav').height() + 30) + 'px)');
 			} else {
-				$('#home-slider.home-video').css('height', '100%');
+				var height = $(window).height() * 0.75;
+				$('#home-slider.home-video').css('height', height + 'px');
 			}
 
 		});
@@ -636,7 +637,8 @@
 		if ($(window).width() > 768 ) {
 			$('#home-slider.home-video').css('height', 'calc(' + $(window).height() + 'px - ' + ($('.section-header').height() + $('#primary-nav').height() + $('#property-nav').height() + 30) + 'px)');
 		} else {
-			$('#home-slider.home-video').css('height', '100%');
+			var height = $(window).height() * 0.75;
+			$('#home-slider.home-video').css('height', height + 'px');
 		}
 
 	});
