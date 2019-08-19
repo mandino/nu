277a6275db9b446757017172a2a40c04dad39e87
nicosabio2016 <nico@misfit-inc.com>
2019-08-19 17:20:58 +0800

    Close button for the login portal
    Bug Fix

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -224,7 +224,12 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		</div>
 		<div class="login-portal__trigger--container">
 			<div class="login-portal__trigger">
-				<span>Sign Up Now</span>
+				<span class="login-portal__text">Sign Up Now</span>
+				<div class="login-portal__close-btn">
+					<span class="line-one"></span>
+					<span class="line-two"></span>
+				</div>
+				
 			</div>
 		</div>
 		
@@ -247,13 +252,21 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			<a href="<?php bloginfo('url'); ?>" class="logo droplogo"><img src="<?php echo get_option('cebo_logo'); ?>" alt="<?php echo the_title(); ?>" /></a>
 			<div class="login-portal__trigger--container">
 				<div class="login-portal__trigger--secondary desktop">
-					<span>Sign Up Now</span>
+					<span class="login-portal__text">Sign Up Now</span>
+					<div class="login-portal__close-btn">
+					<span class="line-one"></span>
+					<span class="line-two"></span>
+				</div>
 				</div>
 			</div>
 			
 			<div class="login-portal__trigger--container">
 				<div class="login-portal__trigger--secondary mobile">
-					<span>Sign Up Now</span>
+					<span class="login-portal__text">Sign Up Now</span>
+					<div class="login-portal__close-btn">
+					<span class="line-one"></span>
+					<span class="line-two"></span>
+				</div>
 				</div>
 			</div>
 			
