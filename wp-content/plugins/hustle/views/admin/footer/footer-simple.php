a61af3cc2d50ee35d5d33bf55f177726faa1d42b
jonathan-misfitcdo <jonathan.misfitcdo@gmail.com>
2019-08-21 16:24:49 +0800

    update hustle pro
    update hustle pro

--- wp-content/plugins/hustle/views/admin/footer/footer-simple.php
+++
@@ -1,9 +0,0 @@
-<?php
-// FOOTER: Brand
-$this->render( 'admin/footer/brand' );
-
-// FOOTER: Navigation
-$this->render( 'admin/footer/navigation' );
-
-// FOOTER: Social
-$this->render( 'admin/footer/social' );
