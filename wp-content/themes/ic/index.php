85d54b2862104f97d7327a08f3a9d3d89d25ed1d
jonathan-misfitcdo <jonathan.misfitcdo@gmail.com>
2019-09-04 18:50:21 +0800

    fix redirect buttons on maps
    fix redirect buttons on maps

--- wp-content/themes/ic/index.php
+++ wp-content/themes/ic/index.php
@@ -235,10 +235,10 @@ get_header();
 
 	<ul style="" class="right-links right" id="toggles">
 
-		<li class="dine"><a class="linkerd active" href="<?php bloginfo('url'); ?>/?page_id=505" title="Dining">Eat</a></li>
-		<li class="shop"><a class="linkerd active" href="<?php bloginfo('url'); ?>/?page_id=507" title="Dining">Shop</a></li>
-		<li class="arts"><a class="linkerd active" href="<?php bloginfo('url'); ?>/?page_id=503" title="Dining">Culture</a></li>
-		<li class="sights"><a class="linkerd active" href="<?php bloginfo('url'); ?>/?page_id=509" title="Dining">Landmarks</a></li>
+		<li class="dine"><a class="linkerd active" href="#" data-pins="<?php bloginfo('url'); ?>/?page_id=505" title="Eat">Eat</a></li>
+		<li class="shop"><a class="linkerd active" href="#" data-pins="<?php bloginfo('url'); ?>/?page_id=507" title="Shop">Shop</a></li>
+		<li class="arts"><a class="linkerd active" href="#" data-pins="<?php bloginfo('url'); ?>/?page_id=503" title="Culture">Culture</a></li>
+		<li class="sights"><a class="linkerd active" href="#" data-pins="<?php bloginfo('url'); ?>/?page_id=509" title="Landmarks">Landmarks</a></li>
 
 	</ul>
 
